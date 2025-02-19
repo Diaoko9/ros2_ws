b36239d8f68133e6fe21e34c16d1d11fcf1eb0c5
Diaoko <ahmad.p.92@gmail.com>
2025-02-19 23:24:44 +0330

    a

---
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -0,0 +1,18 @@
+#include "rclcpp/rclcpp.hpp"
+
+class my_first_node
+{
+private:
+    /* data */
+public:
+    my_first_node(/* args */);
+    ~my_first_node();
+};
+
+my_first_node::my_first_node(/* args */)
+{
+}
+
+my_first_node::~my_first_node()
+{
+}
